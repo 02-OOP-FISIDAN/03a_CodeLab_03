3e68f136f2a6f6f9d61624a15b72553cb18daaa9
FISI_DAN <122363317+FISIDAN@users.noreply.github.com>
2023-02-02 17:41:44 +0100

    mein Vorgehen

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,8 +3,19 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
+
     }
+//public MA_Accounting(String name, String firstName, int birthYear, String workID, String role)
+MA_Accounting maacc = new MA_Accounting("Muetze", "Max", 1989, "234456", "Abteilungsleitung");
+MA_Advertising maadv = new MA_Advertising("Muetze", "Maxine", 1992, "244456", "Bereichsleitung");
+MA_Production maprd = new MA_Production("Raschel", "Rudi", 1999, "334456", "Vorarbeiter");
+
+        output(maacc.doYourWork());
+        output(cat.getStringAttributes("#color"));
+
+        output(cat.getAge()); 
+
+
 
     private static void output(String outputStr) {
         System.out.println(outputStr);
---
+++ com/cc/java/Mitarbeiter.java
@@ -0,0 +1,49 @@
+package com.cc.java;
+
+public class Mitarbeiter {
+    protected String name;
+    protected String firstName;
+    protected int birthYear;
+    public String workID;
+    public String department;
+    public String role;
+   
+     
+    public Mitarbeiter(String name, String firstName, int birthYear, String workID, String department, String role) {
+        this.name = name;
+        this.firstName = firstName;
+        this.birthYear = birthYear;
+        this.workID = workID;
+        this.department = department;
+        this.role = role;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public void hasLunch() {
+    System.out.println("Ich bin zur Mittagspause!");
+    }
+
+    public void startsWork() {
+        System.out.println("Meine Arbeitszeit beginnt um 8 Uhr!");
+   
+   
+}
